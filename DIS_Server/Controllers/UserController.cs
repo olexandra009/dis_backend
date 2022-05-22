81371d470fbf6b274fbec9860da6fbd7c3264049
Alexandra <a.r.chumachenko@gmail.com>
2022-05-22 19:49:55 +0300

    Init project

---
+++ DIS_Server/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIS_Server.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        [HttpPost("/login")]
+        public IActionResult Token([FromBody] LoginUserDtO user)
+        {
+            
+        }
+    }
+}
---
+++ DIS_Server/DTO/LoginUserDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Server.DTO
+{
+    public class LoginUserDto
+    {
+    }
+}
